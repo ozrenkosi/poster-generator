787a57e214497ed14f5528c215447e22b89c8eca
Ozren Kosi <ozren.kosi@gmail.com>
2020-06-11 20:30:36 +0200

    Add files via upload

--- components/Line.js
+++ components/Line.js
@@ -2,35 +2,28 @@ class Line {
   constructor(lineWeight) {
     this.lineColor = color(random(0, 255), random(0, 255), random(0, 255), random(150, 180));
     this.lineWeight = lineWeight;
-    this.linePointiness = 0.005;
-    this.linePositionOffsetMAX = 0.35*height;
-    this.lineResolution = 2;
+    this.lineOffsetMAX = 0.6*height;
+    this.lineResolution = width/width;
 
-    this.xPos = random(width/4, 3*width/4);
-    this.yPos = random(height/4, 3*height/4);
+    this.numberOfSubLines = 15;
 
-    this.xOff = 0;
+    this.yPos = random(height/3, 2*height/3);
     this.yOff = 0;
 
     this.noiseOffset = random(0, 1000000);
   }
 
-  show(lineWeight) {
-    noStroke();
-    fill(this.lineColor);
-    if (random(1) > 0.5) {
+  show() {
+    noFill();
+    stroke(this.lineColor);
+    strokeWeight(this.lineWeight);
+    for (let i = 1; i <= this.numberOfSubLines; i++) {
+      beginShape();
       for (this.xPos = 0; this.xPos < width; this.xPos = this.xPos + this.lineResolution) {
-        this.yOff = map(noise(this.noiseOffset), 0, 1, -this.linePositionOffsetMAX, this.linePositionOffsetMAX);
-        this.noiseOffset = this.noiseOffset + this.linePointiness;
-        ellipse(this.xPos, this.yPos + this.yOff, this.lineWeight);
+        this.yOff = map(noise(this.noiseOffset+this.xPos*0.003), 0, 1, -this.lineOffsetMAX + map(i, 1, this.numberOfSubLines, 0, abs(this.yOff)), this.lineOffsetMAX - map(i, 1, this.numberOfSubLines, 0, abs(this.yOff)));
+        vertex(this.xPos, this.yPos + this.yOff);
       }
-    }
-    else {
-      for (this.yPos = 0; this.yPos < height; this.yPos = this.yPos + this.lineResolution) {
-        this.xOff = map(noise(this.noiseOffset), 0, 1, -this.linePositionOffsetMAX, this.linePositionOffsetMAX);
-        this.noiseOffset = this.noiseOffset + this.linePointiness;
-        ellipse(this.xPos + this.xOff, this.yPos, this.lineWeight);
-      }
+      endShape();
     }
   }
 }
