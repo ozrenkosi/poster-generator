3a83c0c016b011034286eaf29a57069a6e4264c7
Ozren Kosi <ozren.kosi@gmail.com>
2020-06-15 13:13:52 +0200

    Add files via upload

--- components/Line.js
+++ components/Line.js
@@ -11,8 +11,7 @@ class Line {
     this.yOff = 0;
 
     this.noiseOffset = random(0, 1000000);
-    this.noiseOffsetOnStart = this.noiseOffset;
-    this.noiseScrollSpeed = 0.007;
+    this.noiseScrollSpeed = 0.0025;
   }
 
   show() {
@@ -23,17 +22,15 @@ class Line {
       this.lineColor.setAlpha(map(i, 1, this.numberOfSubLines, 150, 180));
       stroke(this.lineColor);
 
-      this.noiseOffset = this.noiseOffsetOnStart;
       beginShape();
       for (this.xPos = 0; this.xPos < width; this.xPos = this.xPos + this.lineResolution) {
-        this.yOff = map(noise(this.noiseOffset), 0, 1, -this.lineOffsetMAX, this.lineOffsetMAX);
+        this.yOff = map(noise(this.noiseOffset + this.xPos*this.noiseScrollSpeed), 0, 1, -this.lineOffsetMAX, this.lineOffsetMAX);
         if (this.yOff > 0) {
           vertex(this.xPos, this.yPos + this.yOff - map(i, 1, this.numberOfSubLines, 0, abs(this.yOff)/2));
         }
         else {
           vertex(this.xPos, this.yPos + this.yOff + map(i, 1, this.numberOfSubLines, 0, abs(this.yOff)/2));
         }
-        this.noiseOffset = this.noiseOffset + this.noiseScrollSpeed;
       }
       endShape();
     }
